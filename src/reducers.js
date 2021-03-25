14027fbc1788a9b33b318160f49223023d88b57f
AhMed SaLah <appsxapp@gmail.com>
2021-03-25 15:43:11 +0200

    initial data loaded from external api

--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,4 @@
+import { ADD_TODO, ADD_GOAL, REMOVE_TODO, REMOVE_GOAL, TOGGLE_TODO } from './constants.js';
 // Composite Root Reducer
 export const app = (state = {}, action) => {
 	return {
@@ -9,11 +10,11 @@ export const app = (state = {}, action) => {
 // Todos Reducer Function
 export const todos = (state = [], action) => {
 	switch (action.type) {
-		case 'ADD_TODO':
+		case ADD_TODO:
 			return state.concat([ action.todo ]);
-		case 'REMOVE_TODO':
+		case REMOVE_TODO:
 			return state.filter((todo) => todo.id !== action.id);
-		case 'TOGGLE_TODO':
+		case TOGGLE_TODO:
 			return state.map(
 				(todo) => (todo.id !== action.id ? todo : Object.assign({}, todo, { complete: !todo.complete }))
 			);
@@ -25,9 +26,9 @@ export const todos = (state = [], action) => {
 // Goals Reducer Function
 export const goals = (state = [], action) => {
 	switch (action.type) {
-		case 'ADD_GOAL':
+		case ADD_GOAL:
 			return state.concat([ action.goal ]);
-		case 'REMOVE_GOAL':
+		case REMOVE_GOAL:
 			return state.filter((goal) => goal.id !== action.id);
 		default:
 			return state;
